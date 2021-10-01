b6e9914d67d027268aa6c66ab4d6ead4c13c35c4
Edward Yang <Edward_Yang@asus.com>
2021-10-01 12:56:00 +0800

    install store devtools & import module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import * as appStore from './store/app.reducer';
@@ -14,7 +15,10 @@ import * as appStore from './store/app.reducer';
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    StoreModule.forRoot({ app: appStore.reducer })
+    StoreModule.forRoot({ app: appStore.reducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
